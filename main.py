c622ad74dffbdccab2e71ecf50b8955e9f685f03
maheensalman26 <153820000+maheensalman26@users.noreply.github.com>
2023-12-19 18:51:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ minCounter = 20
 
 cv2.namedWindow('Object_Detector', cv2.WINDOW_NORMAL)
 
+# Parameters for Lucas-Kanade method
+lk_params = dict(winSize=(15, 15),
+                 maxLevel=2,
+                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+prev_gray = None
+prev_pts = None
+
 while True:
     ret, frame = cap.read()
-    #text="No_Foreign_Objects"
     frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_LINEAR)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
@@ -25,44 +32,51 @@ while True:
     cv2.accumulateWeighted(gray, avg, 0.7)
     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
-    # threshold the delta image, dilate the thresholded image to fill
-    # in holes, then find contours on the thresholded image
     thresh = cv2.threshold(frameDelta, 5, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
-    # loop over the contours
     for c in cnts:
-        # if the contour is too small, ignore it
         if cv2.contourArea(c) < 10000:
             continue
 
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         text = "Foreign Object Detected"
 
         cv2.putText(frame, text, (30, 40),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-        motionCounter = motionCounter + 1
+        motionCounter += 1
         if motionCounter >= minCounter:
-            idx = idx + 1
+            idx += 1
             timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
             filename = f'C:/Users/hp/Desktop/FOD internship/time stamp/{timestamp}_{idx}.jpg'
             cv2.imwrite(filename, frame)
             motionCounter = 0
 
-    # display the security feed
+    if prev_gray is not None:
+        # Calculate optical flow
+        pts, status, _ = cv2.calcOpticalFlowPyrLK(prev_gray, gray, prev_pts, None, **lk_params)
+
+        if pts is not None and prev_pts is not None:
+            for i, (new, old) in enumerate(zip(pts, prev_pts)):
+                a, b = new.ravel().astype(int)
+                c, d = old.ravel().astype(int)
+                cv2.line(frame, (a, b), (c, d), (0, 0, 255), 2)
+                cv2.circle(frame, (a, b), 5, (0, 0, 255), -1)
+
+    prev_gray = gray.copy()
+    prev_pts = cv2.goodFeaturesToTrack(prev_gray, mask=None, maxCorners=100, qualityLevel=0.3,
+                                       minDistance=7, blockSize=7)
+
     cv2.imshow("Object_Detector", frame)
     key = cv2.waitKey(1) & 0xFF
 
-    # if the q key is pressed, break from the loop
     if key == ord("q"):
         break
 
-# clear the stream and close windows
 cap.release()
 cv2.destroyAllWindows()
+
