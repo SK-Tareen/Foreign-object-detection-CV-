044b90d09124e01527db368b16753057d3dff56e
maheensalman26 <153820000+maheensalman26@users.noreply.github.com>
2023-12-19 19:19:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -63,40 +63,37 @@ while True:
 
         if pts is not None and prev_pts is not None:
             speeds = []  # List to store speeds of keypoints
-            
+
             for i, (new, old) in enumerate(zip(pts, prev_pts)):
                 a, b = new.ravel().astype(int)
                 c, d = old.ravel().astype(int)
-                
+
                 # Calculate Euclidean distance between points
                 distance = np.sqrt((c - a) ** 2 + (d - b) ** 2)
-                
+
                 # Calculate speed (distance / time)
                 speed = distance  # Assuming each frame is one unit of time (frame-to-frame distance)
                 speeds.append(speed)
-                
+
                 # Calculate angle of motion
                 angle = np.arctan2(d - b, c - a) * 180 / np.pi
                 if angle < 0:
                     angle += 360
-                
-                # Display angle as text in the top-left corner of the frame
-              # Inside the optical flow loop for displaying angle
-                cv2.putText(frame, f"Angle: {angle:.1f} deg", (20, 30),
-                              cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
 
-# Calculate average speed
-if len(speeds) > 0:
-    average_speed = np.mean(speeds)
-    cv2.putText(frame, f"Avg Speed: {average_speed:.2f} px/frame", (20, 70),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+                # Display angle as text at the bottom of the frame
+                cv2.putText(frame, f"Angle: {angle:.1f}", (20, frame.shape[0] - 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
 
-            
             # Calculate average speed
             if len(speeds) > 0:
                 average_speed = np.mean(speeds)
-                cv2.putText(frame, f"Avg Speed: {average_speed:.2f} px/frame", (20, 60),
+                cv2.putText(frame, f"Avg Speed: {average_speed:.2f} px/frame", (20, frame.shape[0] - 50),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+
+    cv2.putText(frame, "Angle", (20, frame.shape[0] - 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+    cv2.putText(frame, "Avg Speed", (20, frame.shape[0] - 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
 
     prev_gray = gray.copy()
     prev_pts = cv2.goodFeaturesToTrack(prev_gray, mask=None, maxCorners=100, qualityLevel=0.3,
@@ -104,6 +101,13 @@ if len(speeds) > 0:
 
     cv2.imshow("Object_Detector", frame)
     key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
 
     if key == ord("q"):
         break
