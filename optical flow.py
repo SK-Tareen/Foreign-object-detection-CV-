b99e009acf5d7500bcf9a108ba4b8376f9083289
maheensalman26 <153820000+maheensalman26@users.noreply.github.com>
2023-12-28 01:24:15 +0500

    Update optical flow.py

--- optical flow.py
+++ optical flow.py
@@ -1,1 +1,6 @@
+import cv2
+import numpy as np
 
+def calculate_optical_flow(prev_gray, gray, prev_pts, frame):
+    pts, status, _ = cv2.calcOpticalFlowPyrLK(prev_gray, gray, prev_pts, None, winSize=(15, 15),
+                                               maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
